3b07b80d649d9062879d634c2998ad2a4602556e
bufgix <ooruc471@yandex.com>
2020-12-31 15:12:43 +0300

    feat: Login page - I

--- src/popup/views/Auth/Login.vue
+++ src/popup/views/Auth/Login.vue
@@ -1,11 +1,40 @@
 <template>
-  <h1>login</h1>
+  <div>
+    <div class="d-flex flex-justify-center flex-items-center header">
+      <VIcon name="passwall-with-text" width="140px" height="32px" />
+    </div>
+    <div class="p-3 pt-4">
+      <form class="login-form d-flex flex-column">
+        <label v-text="'Username'" class="mb-2" />
+        <VFormText
+          v-model="LoginForm.username"
+          size="medium"
+          name="username"
+          v-validate="'required'"
+          placeholder="Your username"
+        />
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Login",
+  data() {
+    return {
+      LoginForm: {
+        username: "",
+        passworld: "",
+      },
+    };
+  },
 };
 </script>
 
-<style></style>
+<style lang="scss">
+.header {
+  height: 95px;
+  border-bottom: 2px solid $color-black;
+}
+</style>
