a86654351af285962b3069ce7067a169502ff0d4
bufgix <ooruc471@yandex.com>
2020-12-31 15:30:19 +0300

    localize: i18n setup

--- babel.config.js
+++ babel.config.js
@@ -7,6 +7,7 @@ module.exports = {
         root: ["./src"],
         alias: {
           "@p": "./src/popup",
+          "@": "./src",
         },
       },
     ],
--- src/popup/main.js
+++ src/popup/main.js
@@ -3,11 +3,13 @@ import "./config";
 
 import App from "./App.vue";
 import router from "@p/router";
+import i18n from "@/i18n";
 import "../styles/app.scss";
 
 /* eslint-disable no-new */
 new Vue({
   router,
+  i18n,
   el: "#app",
   render: (h) => h(App),
 });
