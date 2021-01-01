4ca4e8e93129ce8433cfacd6836c68ab20171878
bufgix <ooruc471@yandex.com>
2021-01-01 19:36:09 +0300

    chore: add icons

--- src/popup/views/Home/index.vue
+++ src/popup/views/Home/index.vue
@@ -3,12 +3,18 @@
     <Header />
     <div class="mx-3">
       <VFormSearch
+        class="mt-2"
         v-model="searchText"
         theme="black"
         size="medium"
         name="search"
         placeholder="Search"
       />
+      <VIcon name="lock" />
+      <VIcon name="credit-card" />
+      <VIcon name="credit-card" />
+      <VIcon name="credit-card" />
+      <VIcon name="credit-card" />
     </div>
     <RouterView />
     <FABButton class="fab" />
