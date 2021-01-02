39b66ea5594574e8a1efeb513b17b6a87b9afa73
bufgix <ooruc471@yandex.com>
2021-01-02 23:52:14 +0300

    chore: change test data

--- src/popup/views/Logins/store.js
+++ src/popup/views/Logins/store.js
@@ -8,42 +8,50 @@ export default {
           id: 1,
           username: "yakuter@gmail.com",
           url: "https://slack.com/",
+          title: "Slack",
         },
         {
           id: 2,
           username: "yakuter@gmail.com",
           url: "https://www.gmail.com/",
+          title: "Gmail",
         },
         {
           id: 5,
           username: "yakuter@gmail.com",
           url: "https://9gag.com/",
+          title: "9GAG",
+        },
+        {
+          id: 4,
+          username: "yakuter@gmail.com",
+          url: "https://www.twitter.com/",
+          title: "Twitter",
         },
         {
           id: 3,
           username: "yakuter@gmail.com",
           url: "https://www.spotify.com/tr/",
-        },
-        {
-          id: 4,
-          username: "yakuter@gmail.com",
-          url: "https://www.stack.com/",
+          title: "Spotify",
         },
 
         {
           id: 6,
           username: "yakuter@gmail.com",
           url: "https://www.gmail.com/",
+          title: "Gmail",
         },
         {
           id: 7,
           username: "yakuter@gmail.com",
           url: "https://www.gmail.com/",
+          title: "Gmail",
         },
         {
           id: 8,
           username: "yakuter@gmail.com",
           url: "https://www.gmail.com/",
+          title: "Gmail",
         },
       ],
     };
