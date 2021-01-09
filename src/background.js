951c6958fea192474aee49f479a0894ca4f62bb9
bufgix <ooruc471@yandex.com>
2021-01-09 17:33:43 +0300

    fix: change manifest file

--- src/background.js
+++ src/background.js
@@ -1,3 +1,5 @@
+const browser = require("webextension-polyfill");
+
 browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   console.log("Hello from the background");
   console.log(browser);
