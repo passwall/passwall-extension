0ef0fd4a0b7fe6c7a6b3257f43f9def43a2d870b
bufgix <ooruc471@yandex.com>
2021-02-19 22:34:42 +0300

    fix: force animation on a secondary monitor

--- src/popup/config.js
+++ src/popup/config.js
@@ -87,3 +87,33 @@ Vue.prototype.$request = async (callback, waitKey, errorCallback = null) => {
     window.wait.end(waitKey)
   }
 }
+
+if (
+  // From testing the following conditions seem to indicate that the popup was opened on a secondary monitor
+  window.screenLeft < 0 ||
+  window.screenTop < 0 ||
+  window.screenLeft > window.screen.width ||
+  window.screenTop > window.screen.height
+) {
+  chrome.runtime.getPlatformInfo(function(info) {
+    if (info.os === 'mac') {
+      const fontFaceSheet = new CSSStyleSheet()
+      fontFaceSheet.insertRule(`
+        @keyframes redraw {
+          0% {
+            opacity: 1;
+          }
+          100% {
+            opacity: .99;
+          }
+        }
+      `)
+      fontFaceSheet.insertRule(`
+        html {
+          animation: redraw 1s linear infinite;
+        }
+      `)
+      document.adoptedStyleSheets = [...document.adoptedStyleSheets, fontFaceSheet]
+    }
+  })
+}
