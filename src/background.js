61a909bd84808590cb6bd9045824d844af6f0bac
Erhan Yakut <yakuter@gmail.com>
2021-04-02 16:28:38 +0300

    auto catch and fill begins

--- src/background.js
+++ src/background.js
@@ -1,6 +1,64 @@
 const browser = require('webextension-polyfill')
 
-browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  console.log('Hello from the background')
-  console.log(browser)
-})
+// Login Hashmap
+let loginHashmap = new Map()
+
+// Listener
+browser.runtime.onMessage.addListener(handleMessage);
+
+// Handler
+function handleMessage(request, sender, sendResponse) {
+  let url     = domainFromUrl(sender.tab.url);
+  let key     = url + ":" + request.username;
+  let status  = "No new record"
+
+  // Create new record  
+  if (typeof request.username !== 'undefined') {
+    if (!loginHashmap.has(key)) {
+      loginHashmap.set(key, {
+        title: sender.tab.title,
+        url: url,
+        username: request.username,
+        password: request.password,
+      });
+      status = "New record added";
+    }
+  }
+
+  sendResponse({
+    response: status,
+  });
+}
+
+//onUpdated listener fired when a tab URL is changed
+browser.tabs.onUpdated.addListener(handleUpdated);
+
+function handleUpdated(tabId, changeInfo, tabInfo) {
+  let url = domainFromUrl(tabInfo.url);
+  console.log(url);
+
+  // Bu üçü de sistemde yoksa yeni login aç
+  // url     : sender.tab.url,
+  // username: request.username,
+  // password: request.password,
+
+  // Bu üçü de sistemde var ve aynıysa hiç popup çıkarma
+
+  // url ve username ayni ve parola farkliysa update olmali
+
+
+
+  if (tabInfo.active) {
+    // console.log(`handleUpdates : ${tabInfo.url}`);
+  }
+
+  // if (loginHashmap.has(tabInfo.url)) {
+  //   console.log(loginHashmap.get(tabInfo.url))
+  // }
+}
+
+function domainFromUrl(url) {
+  const matches = url.match(/^(?:https?:)?(?:\/\/)?([^\/\?]+)/i)
+  return matches ? matches[1] : 'NONE'
+}
+
--- src/content-scripts/content-script.js
+++ src/content-scripts/content-script.js
@@ -1,4 +1,27 @@
 const browser = require('webextension-polyfill')
 
-console.log('Hello from the content-script')
-console.log(browser)
+console.log("Passwall started ;)")
+
+let inputs = document.querySelectorAll('input'), i;
+let username = "";
+let password = "";
+
+for (i = 0; i < inputs.length; ++i) {
+    if (inputs[i].type === "password") {
+        password = inputs[i];
+        username = inputs[i-1];
+    }
+}
+
+if (password !== "" && username !== "") {
+    password.addEventListener('blur', function () {
+        let login = browser.runtime.sendMessage({
+            username: username.value,
+            password: password.value,
+        });
+        login.then(handleResponse, handleError);
+    });
+}
+
+function handleResponse(message) {console.log(`Response: ${message.response}`);}
+function handleError(error) {console.log(`Error: ${error}`);}
